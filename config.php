b6687b5628c5e04abec62a1efa635a81201233d1
Ali AlSaghir <alisalsaghir@gmail.com>
2024-08-19 13:03:55 +0300

    create Database class and refactor code

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+  'database' => [
+    "host" => "localhost",
+    "port" => 3306,
+    "dbname" => "note-app",
+    "charset" => "utf8mb4",
+  ],
+];
--- index.php
+++ index.php
@@ -2,15 +2,10 @@
 
 require "functions.php";
 // require "router.php";
+require "Database.php";
+$config = require "config.php";
 
-$dsn = "mysql:host=localhost;port=3306;dbname=note-app;charset=utf8mb4";
-
-$pdo = new PDO($dsn, 'root', '');
-
-$statement = $pdo->prepare('SELECT * FROM posts');
-
-$statement->execute();
-
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+$db = new Database($config['database']);
+$posts = $db->query('SELECT * FROM posts')->fetchAll();
 
 dd($posts);
