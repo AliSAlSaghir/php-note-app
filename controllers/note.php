5ecdf8f64e67f285edaa09afb4efbd88a56c83b9
Ali AlSaghir <alisalsaghir@gmail.com>
2024-08-19 16:50:01 +0300

    add unauthorized and not found routes to note page and create Response class to enhance using status codes

--- controllers/note.php
+++ controllers/note.php
@@ -7,6 +7,10 @@ $db = new Database($config['database']);
 $query = "SELECT * FROM notes where id= :id";
 $note = $db->query($query, [':id' => $_GET['id']])->fetch();
 
+if (!$note) abort();
+$currentUserId = 2;
+if ($note['user_id'] !== $currentUserId) abort(Response::FORBIDDEN);
+
 
 $heading = "Note";
 
