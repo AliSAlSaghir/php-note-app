8fef2433fa6d17b87e2b687ebf052d9f5de8bc3e
Ali AlSaghir <alisalsaghir@gmail.com>
2024-08-19 10:43:37 +0300

    set mysql and connect through PDO

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
 
 require "functions.php";
-require "router.php";
+// require "router.php";
+
+$dsn = "mysql:host=localhost;port=3306;dbname=note-app;charset=utf8mb4";
+
+$pdo = new PDO($dsn, 'root', '');
+
+$statement = $pdo->prepare('SELECT * FROM posts');
+
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+dd($posts);
